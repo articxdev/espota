ff271031a3f4ebdc0f46c85dc366c5ea89049f85
aceknoxe <214495621+articxdev@users.noreply.github.com>
2026-02-23 19:01:08 +0530

    Bump version to 1.0.6

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 // ============================================================
 
 // Firmware Version
-#define FIRMWARE_VERSION "1.0.5"
+#define FIRMWARE_VERSION "1.0.6"
 #define FIRMWARE_AUTHOR "ESP32 Project"
 
 // ============================================================
